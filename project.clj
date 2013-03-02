f40f1bb3c0ceeaea8cdd639721508b960724536c
Sean Johnson <sean@snootymonkey.com>
2013-03-02 10:25:08 -0500

    Clojure 1.5 goes GA.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [
-    [org.clojure/clojure "1.5.0-alpha5"] ; Lisp on the JVM http://clojure.org/documentation
+    [org.clojure/clojure "1.5.0"] ; Lisp on the JVM http://clojure.org/documentation
     [ring "1.1.8"] ; Web middleware https://github.com/ring-clojure/ring
     [ring/ring-json "0.2.0"] ; handling JSON requests and responses https://github.com/ring-clojure/ring-json
     [compojure "1.1.5"] ; Web routing https://github.com/weavejester/compojure
