c32b28cf0a129bdc6b130a6e74c80660acccca69
Sean Johnson <sean@snootymonkey.com>
2013-06-20 08:23:57 -0400

    Factor out common JSON building pieces for item and collection.

--- src/fcms/views/collections.clj
+++ src/fcms/views/collections.clj
@@ -25,27 +25,6 @@
 (defn- delete-link [coll]
   (common/delete-link (url coll)))
 
-(defn ordered 
-  "Make an ordered array-map of the ordered key-values for the collection"
-  [coll]
-  ;; Iterate over the ordered keys collecting the value from the map for each key (or a blank string),
-  ;; put these into a vector with the key name, and create an array-map out of it
-  (apply array-map (flatten (map #(vec [% (or (coll %) "")]) common/ordered-keys))))
-
-(defn sorted 
-  "Given the an array-map of the ordered keys and a map of the remaining keys, return an array-map
-  that has the remaining keys sorted and appended to the ordered keys"
-  [ordered, remaining]
-  ;; sort the remaining key-value pairs into a sorted map
-  (let [sorted-remaining (apply sorted-map (flatten (into [] remaining)))]
-    ;; create a new ordered array map from our existing ordered map and the newly sorted map
-    (apply array-map (concat (flatten (into [] ordered)) (flatten (into [] sorted-remaining))))))
-
-(defn remaining 
-  "Remove all the ordered keys leaving just the remaining keys"
-  [coll]
-  (apply dissoc coll common/ordered-keys))
-
 (defn links [coll]
   "Add the HATEAOS links to the collection"
   (apply array-map (concat (flatten (into [] coll)) [:links [
@@ -64,4 +43,5 @@
   ;; 3) adding a sorted hash of any remaining keys
   ;; 3) adding the HATEAOS links to the array hash
   (let [coll-props (dissoc coll :id)]
-    (json/generate-string (-> coll-props ordered (sorted ,,, (remaining coll-props)) links))))
+    (json/generate-string
+      (-> coll-props common/ordered (common/append-sorted ,,, (common/remaining-keys coll-props)) links))))
--- src/fcms/views/common.clj
+++ src/fcms/views/common.clj
@@ -22,3 +22,24 @@
 
 (defn delete-link [url]
   (array-map :rel "delete" :method DELETE :href url))
+
+(defn ordered
+  "Make an ordered array-map of the ordered key-values for the property map"
+  [prop-map]
+  ;; Iterate over the ordered keys collecting the value from the map for each key (or a blank string),
+  ;; put these into a vector with the key name, and create an array-map out of it
+  (apply array-map (flatten (map #(vec [% (or (prop-map %) "")]) ordered-keys))))
+
+(defn append-sorted
+  "Given the an array-map of the ordered keys and a map of the remaining keys, return an array-map
+  that has the remaining keys sorted and appended to the ordered keys"
+  [prop-map, remaining-map]
+  ;; sort the remaining key-value pairs into a sorted map
+  (let [sorted-remaining (apply sorted-map (flatten (into [] remaining-map)))]
+    ;; create a new ordered array map from our existing ordered map and the newly sorted map
+    (apply array-map (concat (flatten (into [] prop-map)) (flatten (into [] sorted-remaining))))))
+
+(defn remaining-keys
+  "Remove all the ordered keys leaving just the remaining keys"
+  [prop-map]
+  (apply dissoc prop-map ordered-keys))
