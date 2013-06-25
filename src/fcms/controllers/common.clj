11aa75c5999fbdebe9e288272bc1ad04a513dc35
Sean Johnson <sean@snootymonkey.com>
2013-06-25 05:49:26 -0400

    Added logging support. Added exception handling and logging to JSON parsing.

--- src/fcms/controllers/common.clj
+++ src/fcms/controllers/common.clj
@@ -1,13 +1,20 @@
 (ns fcms.controllers.common
-  (:require [clojure.walk :refer (keywordize-keys)]
+  (:require [taoensso.timbre :refer (debug info warn error fatal spy)]
+            [clojure.walk :refer (keywordize-keys)]
             [clj-json.core :as json]))
 
+(def malformed true)
+(def good-json false)
+
 (defn malformed-json?
-  "The request body can only be read once, so read in the body param from the request as
-  a string, parse it into JSON, make sure all the keys are keywords, and return it as
-  the 2nd element in a vector, with the first part as false, indicating it's not malformed,
-  otherwise return true to indicate it's malformed."
+  "Read in the body param from the request as a string, parse it into JSON, make sure all the
+  keys are keywords, and then return it, mapped to :data as the 2nd value in a vector,
+  with the first value indicating it's not malformed. Otherwise just indicate it's malformed."
   [ctx]
-  (if-let [data (-> (get-in ctx [:request :body]) slurp json/parse-string keywordize-keys)]
-    [false {:data data}]
-    true))
+  (try 
+    (if-let [data (-> (get-in ctx [:request :body]) slurp json/parse-string keywordize-keys)]
+      [good-json {:data data}]
+      malformed)
+    (catch Exception e
+      (debug "Request body not processable as JSON: " e)
+      malformed)))
