4222af051b67d3839ec200dd3c493541b717ad82
Sean Johnson <sean@snootymonkey.com>
2013-07-13 10:52:35 -0400

    Code cleanup.

--- test/fcms/lib/http_mock.clj
+++ test/fcms/lib/http_mock.clj
@@ -5,16 +5,13 @@
 (def resp) ; mock HTTP response
 
 (defn request
-  ""
   ([] req)
   ([new-req] (def req new-req)))
 
 (defn body
-  ""
   ([] bod)
   ([new-body] (def bod new-body)))
 
 (defn response
-  ""
   ([] resp)
   ([new-resp] (def resp new-resp)))
