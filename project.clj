8362625d72a720554b9f516a5fdaba5cb6423f51
Sean Johnson <sean@snootymonkey.com>
2013-10-19 09:44:43 -0400

    Moved REST API docs to accomodate other API docs.

--- project.clj
+++ project.clj
@@ -12,14 +12,14 @@
     [org.clojure/core.match "0.2.0"] ; Erlang-esque pattern matching https://github.com/clojure/core.match
     [org.clojure/clojurescript "0.0-1934"] ; ClojureScript compiler https://github.com/clojure/clojurescript
     [cheshire "5.2.0"] ; JSON de/encoding https://github.com/dakrone/cheshire
-    [org.flatland/ordered "1.5.1"] ; Ordered hash map https://github.com/flatland/ordered
+    [org.flatland/ordered "1.5.2"] ; Ordered hash map https://github.com/flatland/ordered
     [ring/ring-jetty-adapter "1.2.0"] ; Web Server https://github.com/ring-clojure/ring
     [compojure "1.1.5"] ; Web routing https://github.com/weavejester/compojure
     [liberator "0.9.0"] ; WebMachine (REST state machine) port to Clojure https://github.com/clojure-liberator/liberator
     [com.ashafa/clutch "0.4.0-RC1"] ; CouchDB client https://github.com/clojure-clutch/clutch
     [clojurewerkz/elastisch "1.3.0-beta5"] ; Client for ElasticSearch https://github.com/clojurewerkz/elastisch
     [environ "0.4.0"] ; Get environment settings from different sources https://github.com/weavejester/environ
-    [com.taoensso/timbre "2.6.2"] ; Logging https://github.com/ptaoussanis/timbre
+    [com.taoensso/timbre "2.6.3"] ; Logging https://github.com/ptaoussanis/timbre
   ]
   
   :profiles {
@@ -92,6 +92,7 @@
     [lein-git-deps "0.0.1-SNAPSHOT"] ; dependencies from GitHub https://github.com/tobyhede/lein-git-deps
     [lein-cljsbuild "0.3.4"] ; ClojureScript compiler https://github.com/emezeske/lein-cljsbuild
     [lein-cucumber "1.0.2"] ; BDD testing https://github.com/nilswloka/lein-cucumber
+    [codox "0.6.6"] ; Generate Clojrue API docs https://github.com/weavejester/codox
     [lein-midje "3.1.3-RC1"] ; Example-based testing https://github.com/marick/lein-midje
     [lein-bikeshed "0.1.3"] ; Check for code smells https://github.com/dakrone/lein-bikeshed
     [lein-pprint "1.1.1"] ; pretty-print the lein project map https://github.com/technomancy/leiningen/tree/master/lein-pprint
@@ -103,6 +104,11 @@
     ".lein-git-deps/liberator/src/"
     "src/"
   ]
+
+  :codox {
+    :sources ["src/fcms/resources"]
+    :output-dir ["docs/API/Clojure"]
+  }
 
   ;; ClojureScript
 
