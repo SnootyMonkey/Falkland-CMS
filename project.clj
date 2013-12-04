4e51099e7724d0e7edb218cf15299bd641b90fee
Sean Johnson <sean@snootymonkey.com>
2013-12-04 04:25:54 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
     [com.ashafa/clutch "0.4.0-RC1"] ; CouchDB client https://github.com/clojure-clutch/clutch
     [clojurewerkz/elastisch "1.3.0-rc2"] ; Client for ElasticSearch https://github.com/clojurewerkz/elastisch
     [environ "0.4.0"] ; Get environment settings from different sources https://github.com/weavejester/environ
-    [com.taoensso/timbre "3.0.0-RC1"] ; Logging https://github.com/ptaoussanis/timbre
+    [com.taoensso/timbre "3.0.0-RC2"] ; Logging https://github.com/ptaoussanis/timbre
   ]
   
   :profiles {
