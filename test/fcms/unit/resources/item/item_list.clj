0fab45c08f97f4adc2f873af2ebaa77a02a654df
Sean Johnson <sean@snootymonkey.com>
2013-12-05 06:08:29 -0500

    WIP on item listing unit tests.

--- test/fcms/unit/resources/item/item_list.clj
+++ test/fcms/unit/resources/item/item_list.clj
@@ -7,5 +7,39 @@
 (with-state-changes [(before :facts (empty-collection-e))
                      (after :facts (collection/delete-collection e))]
 
-  (future-facts "about item listing failures")
-  (future-facts "about listing items"))
+  (facts "about item listing failures"
+
+ 	  (facts "when the specified collection doesn't exist"
+      (doseq [coll-slug (conj bad-strings "not-here")]
+        (all-items coll-slug) => :bad-collection)))
+
+  (facts "about listing items"
+
+  	(fact "returns an empty vector when the collection is empty"
+  		(all-items e) => []))
+
+	(with-state-changes [(before :facts (existing-item-i))]
+
+		(fact "returns one item when there is one item in the collection"
+			(count (all-items e)) => 1)
+
+		(with-state-changes [(before :facts (create-item e slug))]
+
+			(fact "returns two items when there are two item in the collection"
+				(count (all-items e)) => 2)
+
+      (future-fact "the item ids are strings")
+
+      (future-fact "the containing collection's slug is included in the items")
+
+      (future-fact "the items' slugs are included")
+
+      (future-fact "the items' names are included")
+
+      (future-fact "the items' descriptions are included")
+
+      (future-fact "the items' versions are 1")
+
+      (future-fact "the custom property of an item in the list is included")
+
+      (future-fact "the item's timestamps are included"))))
