5ef95c43289cde5bf7b3d2c42857cce427c51507
Chris Bui <chris@weare.ninja>
2014-06-24 01:39:49 -0500

    Rename namespace in item_delete.clj to proper name

--- test/fcms/integration/rest_api/item/item_delete.clj
+++ test/fcms/integration/rest_api/item/item_delete.clj
@@ -1,4 +1,4 @@
-(ns fcms.integration.rest-api.item.item-create
+(ns fcms.integration.rest-api.item.item-delete
   (:require [midje.sweet :refer :all]
             [fcms.lib.resources :refer :all]
             [fcms.lib.rest-api-mock :refer :all]
---
+++ test/fcms/integration/rest_api/item/item_update.clj
@@ -0,0 +1,7 @@
+(ns fcms.integration.rest-api.item.item-update
+  (:require [midje.sweet :refer :all]
+            [fcms.lib.resources :refer :all]
+            [fcms.lib.rest-api-mock :refer :all]
+            [fcms.resources.collection :as collection]
+            [fcms.resources.collection-resource :as resource]
+            [fcms.resources.item :as item]))
