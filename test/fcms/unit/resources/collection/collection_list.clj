f4954f099c4a572941d95a129a509f0bf1becf50
Chris Bui <chris@weare.ninja>
2014-06-26 01:10:33 -0500

    Add unit tests for collection_list

--- test/fcms/unit/resources/collection/collection_list.clj
+++ test/fcms/unit/resources/collection/collection_list.clj
@@ -1,1 +1,46 @@
+(ns fcms.unit.resources.collection.collection-list
+  (:require [midje.sweet :refer :all]
+            [fcms.lib.resources :refer :all]
+            [fcms.lib.check :refer (about-now?)]
+            [fcms.resources.collection :refer :all]
+            [fcms.resources.common :as common]))
 
+(defn- setup []
+  (create-collection "custom" {:slug "custom" :description ascii-description :custom foo})
+  (create-collection "coll" {:slug "coll"}))
+
+(defn- teardown []
+  (delete-collection "custom")
+  (delete-collection "coll"))
+
+(facts "about listing collections"
+       (fact "when the specified collection doesn't exist"
+             (doseq [coll-slug (conj bad-strings "not-here")]
+               (get-collection coll-slug) => nil))
+       (facts "about listing collections"
+              (fact "returns an empty vector when there are no collections"
+                    (all-collections) => [])
+              (with-state-changes [(before :facts (reset-collection e))
+                                   (after :facts (delete-collection e))]
+                (fact "returns one collection when there is one collection"
+                      (count (all-collections)) => 1))
+              (facts "about two collections"
+                     (with-state-changes [(before :facts (setup))
+                                          (after :facts (teardown))]
+                       (fact "returns two collections when there are two"
+                             (count (all-collections)) => 2)
+                       (fact "the containers' slugs are included"
+                             (set (map :slug (all-collections))) => #{"custom" "coll"})
+                       (fact "the containers' names are included"
+                             (set (map :name (all-collections))) => #{"custom" "coll"})
+                       (fact "the containers' descriptions are included"
+                             (set (map :description (all-collections))) => #{ascii-description nil}
+                       (fact "the containers' version are 1"
+                             (set (map :version (all-collections))) => #{1})
+                       (fact "the custom property of a collection in the list is included"
+                             (set (map :custom (all-collections))) => #{foo nil})
+                       (fact "the collections' timestamps are included"
+                             (doseq [coll (all-collections)]
+                               (instance? timestamp (:created-at coll)) => true
+                               (about-now? (:created-at coll)) => true
+                               (:created-at coll) => (:updated-at coll))))))))
--- test/fcms/unit/resources/collection/collection_retrieval.clj
+++ test/fcms/unit/resources/collection/collection_retrieval.clj
@@ -11,7 +11,7 @@
          (fact "when the specified collection doesn't exist"
                (doseq [coll-slug (conj bad-strings "not-here")]
                  (get-collection coll-slug) => nil))
-         (with-state-changes [(before :facts (create-collection "custom" {:custom foo}))
+         (with-state-changes [(before :facts (create-collection "custom" {:slug "custom" :custom foo :description ascii-description}))
                               (after :facts (delete-collection "custom"))]
            (facts "about fetching a collection sucessfully"
                   (let [coll (get-collection "custom")]
@@ -21,11 +21,13 @@
                           (:version coll) => 1)
                     (fact "its name is returned"
                           (:name coll) => "custom")
+                    (fact "its description is returned"
+                          (:description coll) => ascii-description)
                     (fact "its slug is returned"
                           (:slug coll) => "custom")
                     (fact "its custom properties are returned"
                           (:custom coll) => foo)
-                    (fact "its timestamps are turned"
+                    (fact "its timestamps are returned"
                           (instance? timestamp (:created-at coll)) => true
                           (about-now? (:created-at coll)) => true
                           (:created-at coll) => (:updated-at coll)))))))
