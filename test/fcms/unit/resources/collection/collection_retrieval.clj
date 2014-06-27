176568073ab2f11ca9403706289c0d4369b49a50
Sean Johnson <sean@snootymonkey.com>
2014-06-27 06:46:07 -0400

    Cleaning up collection retrieval unit tests.

--- test/fcms/unit/resources/collection/collection_retrieval.clj
+++ test/fcms/unit/resources/collection/collection_retrieval.clj
@@ -5,29 +5,52 @@
             [fcms.resources.collection :refer :all]
             [fcms.resources.common :as common]))
 
-(with-state-changes [(before :facts (reset-collection e))
-                     (after :facts (delete-collection e))]
-  (facts "about fetching a collection"
-         (fact "when the specified collection doesn't exist"
-               (doseq [coll-slug (conj bad-strings "not-here")]
-                 (get-collection coll-slug) => nil))
-         (with-state-changes [(before :facts (create-collection "custom" {:slug "custom" :custom foo :description ascii-description}))
-                              (after :facts (delete-collection "custom"))]
-           (facts "about fetching a collection sucessfully"
-                  (let [coll (get-collection "custom")]
-                    (fact "the id is a string"
-                          (instance? String (:id coll)) => true)
-                    (fact "the version is 1"
-                          (:version coll) => 1)
-                    (fact "its name is returned"
-                          (:name coll) => "custom")
-                    (fact "its description is returned"
-                          (:description coll) => ascii-description)
-                    (fact "its slug is returned"
-                          (:slug coll) => "custom")
-                    (fact "its custom properties are returned"
-                          (:custom coll) => foo)
-                    (fact "its timestamps are returned"
-                          (instance? timestamp (:created-at coll)) => true
-                          (about-now? (:created-at coll)) => true
-                          (:created-at coll) => (:updated-at coll)))))))
+(fact "about unsuccessful collection retrieval when the specified collection doesn't exist"
+  (reset-collection e)
+  (doseq [coll-slug (conj bad-strings "not-here")]
+    (get-collection coll-slug) => nil)
+  (delete-collection e))
+
+(with-state-changes [(after :facts (delete-collection "custom"))]
+           
+  (facts "about collection retrieval"
+
+    (fact "successfully"
+      (create-collection "custom" {:slug "custom" :custom foo :description ascii-description})
+      (let [coll (get-collection "custom")]
+        (fact "the id is a string"
+          (instance? String (:id coll)) => true)
+        (fact "the version is 1"
+          (:version coll) => 1)
+        (fact "its name is returned"
+          (:name coll) => "custom")
+        (fact "its description is returned"
+          (:description coll) => ascii-description)
+        (fact "its slug is returned"
+          (:slug coll) => "custom")
+        (fact "its custom properties are returned"
+          (:custom coll) => foo)
+        (fact "its timestamps are returned"
+          (instance? timestamp (:created-at coll)) => true
+          (about-now? (:created-at coll)) => true
+          (:created-at coll) => (:updated-at coll))))
+
+    (fact "with a collection containing unicode"
+      (create-collection unicode-name {:slug "custom" :custom mixed-name :description unicode-description})
+      (let [coll (get-collection "custom")]
+        (fact "the id is a string"
+          (instance? String (:id coll)) => true)
+        (fact "the version is 1"
+          (:version coll) => 1)
+        (fact "its name is returned"
+          (:name coll) => unicode-name)
+        (fact "its description is returned"
+          (:description coll) => unicode-description)
+        (fact "its slug is returned"
+          (:slug coll) => "custom")
+        (fact "its custom properties are returned"
+          (:custom coll) => mixed-name)
+        (fact "its timestamps are returned"
+          (instance? timestamp (:created-at coll)) => true
+          (about-now? (:created-at coll)) => true
+          (:created-at coll) => (:updated-at coll))))))
