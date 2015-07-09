b6ecf6dbc856bee68aaacf3b540c06efdf7ffe25
Sean Johnson <sean@snootymonkey.com>
2015-07-09 06:52:01 -0400

    Dependency update.

--- project.clj
+++ project.clj
@@ -44,7 +44,7 @@
         :hot-reload false
       }
       :dependencies [
-        [midje "1.7.0-SNAPSHOT"] ; Example-based testing https://github.com/marick/Midje
+        [midje "1.7.0"] ; Example-based testing https://github.com/marick/Midje
         [ring-mock "0.1.5"] ; Test Ring requests https://github.com/weavejester/ring-mock
       ]
       :plugins [
